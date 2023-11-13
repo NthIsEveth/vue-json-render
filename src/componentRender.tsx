4addf742a7be1446c629521d9307762d53590cfb
liuzhanshuo <843740469@qq.com>
2023-11-13 18:50:47 +0800

    引入antdv#动态渲染组件

--- src/componentRender.tsx
+++ src/componentRender.tsx
@@ -5,8 +5,9 @@ export type ComponentJson = {
   vmodel: string,
   on?: Function,
   props?: Function,
+  attrs?: Record<string, string>,
   span?: number,
-  [k:string]: any,
+  [k: string]: any,
 }
 
 export default {
@@ -22,13 +23,20 @@ export default {
       default: true,
     }
   },
-  setup(props: any, { expose }: any) {
+  setup(props: any, { expose, h }: any) {
     // 初始化表单数据
-    const { components, needValidate } = props; 
+    const { components, needValidate } = props;
     const originModel: Record<string, any> = {};
     components.forEach((item: ComponentJson) => { originModel[item.vmodel] = undefined })
     const model = reactive(originModel);
-    // const forms = components.map((item: ComponentJson) => needValidate ? (<Form ></Form>))
-    return () => <div></div>
+    const forms = components.map(({ element, props, on, span, vmodel }: ComponentJson) => needValidate
+      ? (
+        
+        <Form model={model[vmodel]}>
+          <FormItem>{h(element, { props, on })}</FormItem>
+        </Form>
+      )
+      : );
+    return () => 
   },
 };
